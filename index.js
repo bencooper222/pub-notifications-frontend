d07b8c6fdcc7a7ebffeabf5c767cefd60db747f6
Benjamin Cooper <bencooper222@gmail.com>
2017-09-18 03:11:16 -0500

    Added firebase storage

--- index.js
+++ index.js
@@ -3,63 +3,96 @@ var restler = require('restler');
 var twilio = require('twilio');
 var config = require('./config');
 var intersect = require('array-intersection');
+var firebase = require('firebase');
 
 
+firebase.initializeApp(config.firebase);
+var database = firebase.database();
+var ref = database.ref('vals');
 
-var numberPairs = {
-    "310": "8152809862",
-    "306": "2246594934"
+var numberPairs = {};
+
+function errData(data) {
+    console.log(data);
 }
 
-function requestPub(processNumbers){
-        restler.get("http://campusdining.vanderbilt.edu/?action=cmi_yoir&request=orderqueue_ajax&location_id=752"
-    ).on('success', function(result, response){
+function updateDatabaseOrders() {
+
+    ref.on('value', function(data) {
+        numberPairs = data.val();
+        //console.log(numberPairs);
+
+    }, errData);
+}
+
+
+updateDatabaseOrders();
+
+
+/*
+ * Gets information from the pub on the orders that are up
+ */
+function requestPub(processNumbers) {
+    console.log("hey");
+    restler.get("http://campusdining.vanderbilt.edu/?action=cmi_yoir&request=orderqueue_ajax&location_id=752").on('success', function(result, response) {
         //console.log(result);
         var orders = JSON.parse(result);
         var orderNumbers = [];
 
-        for(var i=0; i<orders.length; i++){
+        for (var i = 0; i < orders.length; i++) {
             orderNumbers.push(orders[i]["order_id"]);
         }
-        
+
         processNumbers(orderNumbers, handleMatchedNumbers);
 
     });
 
 }
 
-function checkNumbers(allNumbers, twilioNumbers){
+/*
+ * Checks all numbers that are part of the numbers that should be searched for
+ */
+function checkNumbers(allNumbers, handleMatchedNumbers) {
     var searchNumbers = Object.keys(numberPairs);
 
 
-    var foundOrderNumbers = intersect(allNumbers,searchNumbers);
-    
-    twilioNumbers( foundOrderNumbers);
+    var foundOrderNumbers = intersect(allNumbers, searchNumbers);
+
+    handleMatchedNumbers(foundOrderNumbers);
 }
+
+
+
+
+function textOrder(orderNumber) {
+    var client = new twilio(config.twilio.uid, config.twilio.auth);
+
+    client.messages.create({
+            body: 'Your order, #' + orderNumber + ' is up!',
+            to: numberPairs[orderNumber], // Text this number
+            from: config.twilio.phone // From a valid Twilio number
+        })
+        .then((message) => console.log(message.sid));
+}
+
 
 /*
-* Intersection of two arrays
-*/
+ * Iterates through matched numbers to text them about their order
+ */
+function handleMatchedNumbers(orderNumbers) {
 
-
-function textOrder(orderNumber){
-    var client = new twilio(config.twilioId,config.auth);
-    
-    client.messages.create({
-        body: 'Your order, #' + orderNumber+ ' is up!' ,
-        to: numberPairs[orderNumber],  // Text this number
-        from: config.phone // From a valid Twilio number
-    })
-    .then((message) => console.log(message.sid));
-}
-
-
-function handleMatchedNumbers(orderNumbers){
-
-    for(var i=0; i<orderNumbers.length;i++){
+    for (var i = 0; i < orderNumbers.length; i++) {
         //textOrder(orderNumbers[i]);
         console.log(orderNumbers[i]);
+      
+
+        database.ref('vals/' + orderNumbers[i]).remove() // finished with order, get rid of it
+            .then(function() {
+                console.log("Remove succeeded.")
+            })
     }
 }
 
-requestPub(checkNumbers);
+//requestPub(checkNumbers)
+handleMatchedNumbers([201]);
+//setInterval(requestPub(checkNumbers), 1500);
