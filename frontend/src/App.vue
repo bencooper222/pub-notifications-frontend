f29a2caa4f798d64dd2021bd63aa1bbc2fa0bb65
Benjamin Cooper <bencooper222@gmail.com>
2017-09-19 18:59:27 -0500

    Final touchs for build

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -70,7 +70,6 @@
 
 
                 var xhr = new XMLHttpRequest();
-                xhr.open("POST", 'http://localhost:3000', true);
 
                 //Send the proper header information along with the request
                 xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
