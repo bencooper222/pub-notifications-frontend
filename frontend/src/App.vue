b6bf9163e50735099b33aa57073ad6e6321f428c
Benjamin Cooper <bencooper222@gmail.com>
2017-11-07 01:27:02 -0600

    Quality of life updates

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -40,7 +40,7 @@
         <p class="extra-info">  <a href="https://github.com/bencooper222/pub-notifications">Code</a>&nbsp;|</p>
         <p class="extra-info"><a href="http://campusdining.vanderbilt.edu/?action=cmi_yoir&request=screen&location_id=752">  API</a>
         </p>
-        <p id="love">Made with <span class="red">❤</span> by Ben Cooper</p>
+        <p id="love">Made with <span class="red">❤</span> + <span title="Vanilla lattes">☕</span> by <a href="http://benc.io">Ben Cooper</a></p>
 
     </div>
 </template>
@@ -130,7 +130,7 @@
     }
     
     a {
-        color: #42b983;
+        color: rebeccapurple;
     }
     
     #love {
